43700eadd55a302a2099cbe5852c1dc54f6aa155
aplyd <austin@ftacnik.com>
2019-10-02 15:53:44 -0700

    break everything trying to allow for stringing together multiple operations

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let display = document.querySelector('#display');
 let ac = document.querySelector('#ac');
 let displayValue = [];
+let operateArray = [];
 
 display.textContent = 0;
 
@@ -28,27 +29,30 @@ numButtons.forEach(function(e) {
     e.addEventListener('click', function(event) {
         displayValue.push(event.target.value);
         display.textContent = displayValue.join('');
-
         resetMathButtonStyling();
+
+        operateArray[0] = displayValue.join('');
+        //update operateArray[1] here instead of pulling value from display
+        console.log(operateArray); 
     })
 })
 
 
 //uh make calculator do math hopefully
-let operateArray = [];
 let mathButtons = document.querySelectorAll('.mathButtons');
 
 mathButtons.forEach(function(e) {
     e.addEventListener('click', function(event) {
-        operateArray[0] = displayValue.join('');
+        
         displayValue = [];
-        display.textContent = '';
+        display.textContent = displayValue;
 
         resetMathButtonStyling();
         
         operateArray[1] = event.target.value;
         event.target.style.backgroundColor = "black";
         event.target.style.color = "white";
+        console.log(operateArray);
     })
 })
 
@@ -56,8 +60,8 @@ equalsButton = document.querySelector('#equalsButton');
 
 equalsButton.addEventListener('click', function(event) {
     operateArray[2] = displayValue.join('');
-    operate(operateArray[1], operateArray[0], operateArray[2]);
-
+    display.textContent = operate(operateArray[0], operateArray[1], operateArray[2]);
+    console.log(operateArray);
     resetMathButtonStyling();
 })
 
@@ -67,7 +71,7 @@ let subtract = (a, b) => a - b;
 let divide = (a, b) => a / b;
 let multiply = (a, b) => a * b;
 
-function operate(operator, a, b) {
+function operate(a, operator, b) {
     if (operator === '+') {
         display.textContent = add(a, b);
 
